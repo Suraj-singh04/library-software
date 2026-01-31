916208e375964032f2370da3a0881ddd3179f8c3
Suraj-singh04 <surajsingh877028@gmail.com>
2026-01-31 00:07:12 -0500

    Initialized Project Structre

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,9 +2,11 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL ?? "",
-        },
+    schema: "prisma/schema.prisma",
+    migrations: {
+        path: "prisma/migrations",
     },
+    datasource: {
+        url: process.env.DATABASE_URL!,
+    }
 });
